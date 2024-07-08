ed2fb12ba86e5480a2296c1439e5d63de4df9db8
FTA-RBeaney <robbeaney@gmail.com>
2024-07-08 16:42:16 +0100

    Changed Algolia keys to public

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,8 +36,8 @@ export default defineNuxtConfig({
     pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
   },
   algolia: {
-    apiKey: process.env.ALGOLIA_API_KEY,
-    applicationId: process.env.ALGOLIA_APPLICATION_ID,
+    apiKey: process.env.PUBLIC_ALGOLIA_API_KEY,
+    applicationId: process.env.PUBLIC_ALGOLIA_APPLICATION_ID,
     instantSearch: {
       // theme: "algolia",
     },
