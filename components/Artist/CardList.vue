d6c8b43fc0cd07b1cfe6295c024d981864e70792
FTA-RBeaney <robbeaney@gmail.com>
2024-07-23 16:48:02 +0100

    Musician page now uses dynamic component!

---
+++ components/Artist/CardList.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="(musician, i) in props.musicians"
+        :key="`musician-${i}`"
+        class="flex"
+      >
+        <ArtistCard :artistId="musician.id" :musician="musician" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  musicians: Object,
+});
+</script>
+
+<style scoped lang="scss">
+ul {
+  display: grid;
+  gap: 0.2em;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+}
+
+li {
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+
+  > div > a {
+    max-height: 150px;
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+    padding-bottom: 220px;
+    display: block;
+    object-fit: cover;
+
+    @media (width >= 600px) {
+      max-height: 280px;
+    }
+
+    img {
+      max-width: 100%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      width: 100%;
+      height: 100%;
+      transform: translate(-50%, -50%);
+      object-fit: cover;
+    }
+  }
+}
+.list-view {
+  display: block;
+
+  li > div {
+    display: flex;
+    margin-bottom: 2rem;
+    max-width: 100%;
+    justify-content: flex-start;
+  }
+
+  .card-image {
+    width: 200px;
+    max-height: 200px;
+    padding-bottom: 180px;
+
+    @media (width >= 600px) {
+      max-height: 150px;
+    }
+  }
+}
+</style>
--- pages/musicians.vue
+++ pages/musicians.vue
@@ -62,6 +62,25 @@ await getMusicians();
 onUnmounted(() => {
   supabase.removeChannel(channel);
 });
+
+const ComponentToRender = computed(() => {
+  if (!noResults.value) {
+    return {
+      name: resolveComponent("MusiciansTable"),
+      dataToSend: results.value,
+    };
+  } else if (!isToggled.value && noResults.value) {
+    return {
+      name: resolveComponent("ArtistCardList"),
+      dataToSend: allTheMusic.value,
+    };
+  } else {
+    return {
+      name: resolveComponent("MusiciansTable"),
+      dataToSend: allTheMusic.value,
+    };
+  }
+});
 </script>
 
 <template>
@@ -85,100 +104,11 @@ onUnmounted(() => {
       <div
         class="bg-white border p-6 border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 relative w-full h-full flex flex-col overflow-hidden mt-6"
       >
-        <MusiciansTable v-if="!noResults" :musicians="results" />
-
-        <ul v-if="!isToggled && noResults" class="">
-          <li v-for="musician in allTheMusic" :key="musician" class="flex">
-            <ArtistCard :artistId="musician.id" :musician="musician" />
-          </li>
-        </ul>
-
-        <MusiciansTable
-          v-if="isToggled && noResults"
-          :musicians="allTheMusic"
+        <component
+          :is="ComponentToRender.name"
+          :musicians="ComponentToRender.dataToSend"
         />
-
-        <Table v-if="isToggled && noResults">
-          <TableHeader>
-            <TableRow>
-              <TableHead class="font-bold"> Name </TableHead>
-              <TableHead class="font-bold"> Description </TableHead>
-              <TableHead class="font-bold text-right"> Action </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <ArtistRow
-              v-for="musician in allTheMusic"
-              :key="musician"
-              :artistId="musician.id"
-              :musician="musician"
-            />
-          </TableBody>
-        </Table>
       </div>
     </div>
-    <!-- <ul class="flex justify-between mt-6">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul> -->
   </div>
 </template>
-
-<style scoped lang="scss">
-ul {
-  display: grid;
-  gap: 0.2em;
-  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
-}
-
-li {
-  margin-right: 1rem;
-  margin-bottom: 1rem;
-
-  > div > a {
-    max-height: 150px;
-    overflow: hidden;
-    position: relative;
-    width: 100%;
-    padding-bottom: 220px;
-    display: block;
-    object-fit: cover;
-
-    @media (width >= 600px) {
-      max-height: 280px;
-    }
-
-    img {
-      max-width: 100%;
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      width: 100%;
-      height: 100%;
-      transform: translate(-50%, -50%);
-      object-fit: cover;
-    }
-  }
-}
-.list-view {
-  display: block;
-
-  li > div {
-    display: flex;
-    margin-bottom: 2rem;
-    max-width: 100%;
-    justify-content: flex-start;
-  }
-
-  .card-image {
-    width: 200px;
-    max-height: 200px;
-    padding-bottom: 180px;
-
-    @media (width >= 600px) {
-      max-height: 150px;
-    }
-  }
-}
-</style>
