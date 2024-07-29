a48548e13c14f0debbce74d4e4f015f0593d6ef7
FTA-RBeaney <robbeaney@gmail.com>
2024-07-29 22:59:57 +0100

    New Latest users section on home page

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@ const supabase = useSupabaseClient();
 const isOpen = ref(false);
 
 const userData = ref();
+const listOfUsers = ref([]);
 
 onMounted(async () => {
   const { data } = await supabase
@@ -20,6 +21,10 @@ onMounted(async () => {
     .eq("id", supabaseUser.value.id);
   userData.value = data[0];
 });
+
+const { data: users } = await supabase.from("profiles").select();
+
+listOfUsers.value = users;
 </script>
 
 <template>
@@ -52,16 +57,49 @@ onMounted(async () => {
         </div>
       </div>
     </header>
-    <div
-      class="bg-white border p-6 border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 relative w-10/12 text-center text-sm h-full flex flex-col overflow-hidden mt-6 dark:text-white mx-auto"
-    >
-      <p>
-        Thanks for checking out the site. This is a work in progress, so some
-        things may not work and ther may be bugs. If you find anything or think
-        of anything you'd like to see on this site then feel free to let me know
-        by clicking on 'Add feedback/bug' in the navigation bar above and
-        leaving a note!
-      </p>
+    <div class="flex container justify-between mt-8 max-w-screen-lg mx-auto">
+      <div class="w-8/12">
+        <Card class="p-4">
+          <p>
+            Thanks for checking out the site. This is a work in progress, so
+            some things may not work and there may be bugs. If you find anything
+            or think of anything you'd like to see on this site then feel free
+            to let me know by clicking on 'Add feedback/bug' in the navigation
+            bar above and leaving a note!
+          </p>
+        </Card>
+      </div>
+      <div class="w-4/12 max-w-xs pl-4">
+        <Card class="p-4">
+          <p class="py-2 text-xl font-semibold">Latest legends:</p>
+          <Table v-if="listOfUsers">
+            <TableBody>
+              <TableRow
+                v-for="(user, index) in listOfUsers"
+                :key="`item${index}`"
+              >
+                <TableCell>
+                  <div class="flex justify-start items-center">
+                    <NuxtImg
+                      v-if="user"
+                      class="aspect-square object-cover rounded-full mr-2"
+                      :src="user.avatar_url"
+                      format="webp"
+                      width="30"
+                      height="30"
+                      preload
+                      loading="lazy"
+                      placeholder="https://sternbergclinic.com.au/wp-content/uploads/2020/03/placeholder.png"
+                      :alt="user.first_name"
+                    />
+                    {{ user.first_name }}
+                  </div>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
+      </div>
     </div>
   </div>
 </template>
